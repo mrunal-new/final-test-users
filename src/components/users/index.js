091d198f513c6d5dd799d6a3b3577589bf08b977
mrunal-new <83772319+mrunal-new@users.noreply.github.com>
2021-05-10 22:23:33 +0530

    edit changes

--- src/components/users/index.js
+++ src/components/users/index.js
@@ -11,18 +11,21 @@ const Users = () => {
 
     setFormData({
       ...formData,
-      [name]: value
+      [name]: value,
+
     })
   }
+
+  console.log(users);
 
   const handleUsers = () => {
     if (formData !== undefined) {
       let res = Object.getOwnPropertyNames(formData);
       if (res.length === 5 ) {
         if (users === undefined) {
-          setUsers([ formData ]);
+          setUsers([{...formData, id: Math.random()}]);
         } else {
-          setUsers([ ...users, formData ]);
+          setUsers([ ...users, {...formData, id: Math.random()} ]);
         }
       }
     }
@@ -31,11 +34,37 @@ const Users = () => {
   const handleDeleteUsers = (data) => {
     setUsers((prevState) => {
       const fil = prevState.filter((value) => {
-        return value.name !== data ;
+        return value.id !== data ;
       });
       return fil;
     })
 
+  }
+
+  const handleEditUsers = (data) => {
+    setUsers((prevState) => {
+      let res = prevState.filter((val) => {
+        return val.id === data;
+      })
+      let res2 = prevState.filter((val) => {
+        return val.id !== data;
+      })
+
+      return [ ...res2, { ...res[0], edit: true } ]
+    })
+  }
+
+  const handleInsaneEdit = (id) => {
+    setUsers((prevState) => {
+      let res = prevState.filter((val) => {
+        return val.id === id;
+      })
+      let res2 = prevState.filter((val) => {
+        return val.id !== id;
+      })
+
+      return [ ...res2, { ...formData } ]
+    });
   }
 
   return (
@@ -52,16 +81,34 @@ const Users = () => {
         </tr>
         </thead>
         <tbody>
+          <tr>
+            <td>Loki</td>
+            <td>M</td>
+            <td>294783</td>
+            <td>BE</td>
+            <td>loki@gmail.com</td>
+          </tr>
           {users?.map((value) => (
+            (!value.hasOwnProperty('edit')) ? (
             <tr >
             <td>{value.name}</td>
             <td>{value.gender}</td>
             <td>{value.phone}</td>
             <td>{value.qualification}</td>
             <td>{value.email}</td>
-            <td><Button variant="danger" onClick={() => handleDeleteUsers(value.name)}>Delete</Button></td>
-            <td><Button variant="black" onClick={() => handleDeleteUsers(value.name)}>Edit</Button></td>
-          </tr>
+            <td><Button variant="danger" onClick={() => handleDeleteUsers(value.id)}>Delete</Button></td>
+            <td ><Button variant="light" onClick={() => handleEditUsers(value.id)}>Edit</Button></td>
+            </tr>
+            ) : (
+            <tr>
+            <td><input type="text" defaultValue={value.name} name='name' placeholder="name" onChange={handleForm} /></td>
+            <td><input type="text" defaultValue={value.gender} name='gender' placeholder="gender" onChange={handleForm}/></td>
+            <td><input type="number" defaultValue={value.phone} name='phone' placeholder="phone" onChange={handleForm}/></td>
+            <td><input type="text" defaultValue={value.qualification} name='qualification' placeholder="qualification" onChange={handleForm}/></td>
+            <td><input type="email" defaultValue={value.email} name='email' placeholder="Email" onChange={handleForm}/></td>
+            <td><Button variant="success" onClick={() => handleInsaneEdit(value.id)}>Done</Button></td>
+            </tr>
+            )
           ))}
         </tbody>
         <span>Add user below</span>
